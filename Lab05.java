221636920134efaa9e0e3f317c8d6cb911caf080
Andre17 <andrepurits@Andre-Puritss-MacBook-Pro-3.local>
2014-11-11 15:05:51 +0000

    Lab 05

--- Lab05.java
+++ Lab05.java
@@ -2,16 +2,39 @@
 *
 * Description of the program goes here  // provide a brief description
 *
-* @author <Your Name here>  // replace <...> with your name
-* @version <date/of/completion> // replace <...> with the date
+* @author <Andre Purits>  // replace <...> with your name
+* @version <11/11/14> // replace <...> with the date
 */
 
 import edu.fcps.karel2.Display;
 import edu.fcps.karel2.Robot;
 
 public class Lab05 {
-
+   public static void runTheRace(Racer arg) {
+   arg.move();
+   arg.jumpRight();
+   arg.shuttle(1,7);
+   arg.turnAround();
+   arg.shuttle(3,5);
+   arg.turnAround();
+   arg.shuttle(5,3);
+   arg.turnAround();
+   arg.jumpLeft();
+   arg.turnAround();
+   arg.move();
+   arg.put(15);
+   arg.turnAround();
+   arg.move();
+   }
      public static void main(String[] args) {
-          // TODO Your code goes here
+          Display.openWorld("maps/shuttle.map");
+          Display.setSize(10,10);
+          Display.setSpeed(8);
+          Racer andre = new Racer(1);
+          Racer giroud = new Racer(4);
+          Racer zlatan = new Racer(7);
+          runTheRace(andre);
+          runTheRace(giroud);
+          runTheRace(zlatan);
      }
 }
--- Racer.java
+++ Racer.java
@@ -1,12 +1,56 @@
 /**
 *  <Describe that the Racer class does>
 *
-*  @author <your name here, no brackets>
-*  @version <version or date, no brackets>
+*  @author <Andre Purits>
+*  @version <11/11/14>
 */
+import edu.fcps.karel2.Display;
+import edu.fcps.karel2.Robot;
 
-public class Racer extends Athlete {
+public class Racer extends Athlete 
+{
+public Racer (int y)
+{
+   super(1, y, Display.EAST, Display.INFINITY);
+}
+public void jumpRight(){
+turnLeft();
+move();
+turnRight();
+move();
+turnRight();
+move();
+turnLeft();
+}
+public void jumpLeft() {
+turnLeft();
+move();
+turnLeft();
+move();
+turnLeft();
+move();
+turnLeft();
+}
+public void sprint(int n) {
+   for(int k=1; k <=n; k++)
+move();
+}
+public void put(int n) {
+for (int k=1; k <= n; k++)
+      putBeeper();
+}
+public void pick(int n) {
+for(int k=1; k <= n; k++)
+pickBeeper();
+}
 
-// consult the Karel API to determine what constructors and methods this class should implement
-
+public void shuttle(int spaces,int beepers) {
+for(int k=0; k <= spaces; k++)
+move();
+for(int k=1; k <= beepers; k++)
+   pickBeeper();
+   turnAround();
+   for(int k=0; k <= spaces; k++)
+   move();
+   }
 }
